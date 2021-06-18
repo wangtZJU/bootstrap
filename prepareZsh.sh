58b2ffcbc4bcbdeed1d9a3a99e681bf435e13dca
Wang Tao <wangtao.2077@bytedance.com>
2021-06-18 20:51:26 +0800

    fix

--- prepareZsh.sh
+++ prepareZsh.sh
@@ -5,13 +5,14 @@ source utils.sh
 
 install_package "zsh"
 
-if [[ -z "$ZSH" ]]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if which zsh; then
+    yes_or_no "no" "Change shell?(y/n, default: n)"
+    if [[ "$yn_choice" == "yes" ]]; then
+        chsh -s /bin/zsh
+    fi
 fi
 
-yes_or_no "no" "Change shell?(y/n, default: n)"
-
-if [[ "$yn_choice" == "yes" ]]; then
-    chsh -s /bin/zsh
+if [[ -z "$ZSH" ]]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 

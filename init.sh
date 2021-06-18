3435d08751cd348c222205bf58dc0e805543713d
Wang Tao <wangtao.2077@bytedance.com>
2021-06-18 20:27:39 +0800

    fix

--- init.sh
+++ init.sh
@@ -60,14 +60,23 @@ configure_profile0 (){
     fi
 
     if [[ -f "$HOME/.profile0" ]]; then
-        echo -e "${COLOR_YELLOW}profile0 file already exists, move it to ~/.profile0.bak${COLOR_NONE}"
-        mv "$HOME/.profile0" "$HOME/.profile0.bak" 
+        echo -e "${COLOR_YELLOW}~/.profile0 already exists, overwrite?${COLOR_NONE}"
+        yes_or_no "no"
+
+        if [[ $yn_choice == "yes" ]]; then
+            mv "$HOME/.profile0" "$HOME/.profile0.bak" 
+            echo -e "${COLOR_GREEN}Move old ~/.profile0 to ~/.profile0.bak${COLOR_NONE}"
+        fi
     fi
 
     cp profile0 "$HOME/.profile0" 
 
     if ! grep -q 'source $HOME/.profile0' "$HOME/.bashrc" 2>/dev/null ; then
         echo 'source $HOME/.profile0' >> $HOME/.bashrc
+    fi
+
+    if ! grep -q 'source $HOME/.profile0' "$HOME/.bash_profile" 2>/dev/null ; then
+        echo 'source $HOME/.profile0' >> $HOME/.bash_profile
     fi
 
     if ! grep -q 'source $HOME/.profile0' "$HOME/.zshrc" 2>/dev/null ; then

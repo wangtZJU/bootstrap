094d6bd8296f4bd3b860b5f58f85bc587700cdaa
Wang Tao <wangtao.2077@bytedance.com>
2022-10-13 14:48:04 +0800

    fix

--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,10 @@
+import os
+import platform as py_platform
+import subprocess
+from enum import Enum
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 class bcolors:
     PURPLE = '\033[95m'
     BLUE = '\033[94m'
@@ -10,18 +17,40 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def log_info(s):
-    print(f"{bcolors.CYAN}{s}{bcolors.ENDC}")
+    print("{}{}{}".format(bcolors.CYAN, s, bcolors.ENDC))
 
 def log_success(s):
-    print(f"{bcolors.GREEN}{s}{bcolors.ENDC}")
+    print("{}{}{}".format(bcolors.GREEN, s, bcolors.ENDC))
 
 def log_warning(s):
-    print(f"{bcolors.YELLOW}{s}{bcolors.ENDC}")
+    print("{}{}{}".format(bcolors.YELLOW, s, bcolors.ENDC))
 
 def log_error(s):
-    print(f"{bcolors.RED}{bcolors.BOLD}{s}{bcolors.ENDC}")
+    print("{}{}{}{}".format(bcolors.RED, bcolors.BOLD, s, bcolors.ENDC))
 
 def exec_recipe(r):
-    exec(open(f"recipes/{r}.py").read())
+    exec(open("{}/recipes/{}.py".format(PROJECT_ROOT, r)).read())
 
+class Platform(Enum):
+    Unknown = 1
+    Darwin = 2
+    Debian = 3
 
+    @staticmethod
+    def detectPlatform():
+        if py_platform.system().lower() == 'darwin':
+            return Platform.Darwin
+        if py_platform.system().lower() == 'linux':
+            if py_platform.linux_distribution()[0].lower() == 'debian':
+                return Platform.Debian
+        return Platform.Unknown;
+
+PLATFORM = Platform.detectPlatform()
+
+def run_command(cmd, **options):
+    print_output = True
+    ignore_error = False
+    if type(cmd) == str:
+        cmd = cmd.split()
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    return process.communicate()
--- recipes/0001-hello-world.py
+++ recipes/0001-hello-world.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import os, sys; sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from lib import utils
+from lib.utils import *
 
-utils.log_info('hello world')
-utils.log_success('hello world')
-utils.log_warning('hello world')
-utils.log_error('hello world')
+log_info('platform is {}'.format(PLATFORM))
 
+output, error = run_command('ls /')
+log_success(output)

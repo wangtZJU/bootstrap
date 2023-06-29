1a095a064671336ccb169894242bed9190895560
Wang Tao <wangtao.2077@bytedance.com>
2023-06-29 17:48:30 +0800

    fix

--- common/python/install-user-py.sh
+++ common/python/install-user-py.sh
@@ -4,7 +4,7 @@ source ../common.sh
 
 PY_VERSION="${PY_VERSION:-3.9}"
 PY_EXEC="python${PY_VERSION}"
-USER_PY_HOME="~/.py${PY_VERSION}"
+USER_PY_HOME="${HOME}/.py${PY_VERSION}"
 
 if ! which $PY_EXEC; then
     echo "Python ${PY_VERSION} not found in path!"
@@ -13,5 +13,5 @@ fi
 
 virtualenv -p ${PY_EXEC} ${USER_PY_HOME}
 
-echo -e "Add ${COLOR_GREEN}VIRTUAL_ENV_DISABLE_PROMPT=1 ${USER_PY_HOME}/bin/active${COLOR_NONE} into your .zshrc/.bashrc"
+echo -e "Add ${COLOR_GREEN}VIRTUAL_ENV_DISABLE_PROMPT=1 source ${USER_PY_HOME}/bin/active${COLOR_NONE} into your .zshrc/.bashrc"
 

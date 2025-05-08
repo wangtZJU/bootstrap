924ef76ce662153562087eaa090dde8799b17852
Wang Tao <wangtao.2077@bytedance.com>
2025-05-08 14:47:14 +0800

    clean

--- v1_legacy/boot-linux-devbox.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-from lib.utils import exec_recipe
-
-exec_recipe('0001-hello-world')
-
--- v1_legacy/recipes/0001-hello-world.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os, sys; sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-
-from lib.utils import *
-
-log_info('platform is {}'.format(PLATFORM))
-
-output, error = run_command('ls /')
-log_success(output)

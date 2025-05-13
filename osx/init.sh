e5a03ea0cf9a5e0641d3df92f8534b3a3d727af0
Wang Tao <wangtao.2077@bytedance.com>
2025-05-13 19:52:11 +0800

    tmp

--- osx/init.sh
+++ osx/init.sh
@@ -16,4 +16,14 @@ if ! which $PY_EXEC &>/dev/null; then
     brew install "python@${PY_VERSION}"
 fi
 
+if ! which $AWK_EXEC &>/dev/null; then
+    echo "Installing $AWK_EXEC..."
+    brew install gawk
+fi
+
+if ! which $SED_EXEC &>/dev/null; then
+    echo "Installing $SED_EXEC..."
+    brew install gnu-sed
+fi
+
 mkdir -p $HOME/Workspace
